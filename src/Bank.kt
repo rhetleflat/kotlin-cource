83487ba58137f720eb76a9b4974541951579ccd9
Robert Hetleflåt <36657327+rhetleflat@users.noreply.github.com>
2021-11-30 07:52:34 +0100

    Add files via upload

---
+++ src/Bank.kt
@@ -0,0 +1,66 @@
+fun renteJustering(kontoListe: ArrayList<Konto>) {
+    println("Oppgi prosent")
+    var percent = readLine()?:""
+    kontoListe.forEach{it.adjust(percent.toDouble())
+    it.printSaldo()}
+}
+
+fun taUtPenger(kontoListe: ArrayList<Konto>) {
+    println("Oppgi kontonr")
+    var kontonr = readLine()?:""
+    var nyKontoListe = kontoListe.filter {konto -> konto.kontonr.equals(kontonr) }
+    if (nyKontoListe.isNotEmpty()) {
+        println("Oppgi beløp du vil ta ut ")
+        var belop = readLine()?:""
+        nyKontoListe.first().substract(belop.toDouble())
+        nyKontoListe.first().printSaldo()
+    }
+}
+
+fun settInnPenger(kontoListe: ArrayList<Konto>) {
+    println("Oppgi kontonr")
+    var kontonr = readLine()?:""
+    var nyKontoListe = kontoListe.filter {konto -> konto.kontonr.equals(kontonr) }
+    if (nyKontoListe.isNotEmpty()) {
+        println("Oppgi innskuddsbeløp ")
+        var belop = readLine()?:""
+        var inskudd = belop.toDouble()
+        nyKontoListe.first().add(inskudd)
+        nyKontoListe.first().printSaldo()
+    }
+}
+
+fun opprettKonto(kontoListe: ArrayList<Konto>) {
+   println("Oppgi navn")
+    var navn = readLine()?:""
+    println("Oppgi adresse")
+    var adresse = readLine()?:""
+    println("Oppgi postnr")
+    var postnr = readLine()?:""
+    println("Oppgi poststed")
+    var poststed = readLine()?:""
+    println("Oppgi kontonr")
+    var kontonr = readLine()?:""
+    var konto = Konto(navn,adresse,postnr,poststed,kontonr,0.0)
+    kontoListe.add(konto)
+}
+
+fun main (args: Array<String>) {
+    var isMeny=true;
+    var kontoListe: ArrayList<Konto> = ArrayList()
+    while (isMeny) {
+        println("1. Opprett bankkonto")
+        println("2. Sett inn penger")
+        println("3. Ta ut penger")
+        println("4. Gjør rentejustering")
+        println("5. Avslutt")
+        var input = readLine()?:""
+        when (input) {
+            "1" -> opprettKonto(kontoListe)
+            "2" -> settInnPenger(kontoListe)
+            "3" -> taUtPenger(kontoListe)
+            "4" -> renteJustering(kontoListe)
+            "5" -> isMeny=false
+        }
+    }
+}
