22040c75940b97197aa53b3eb6407ffa423377c2
rhetleflaat <roberth@kred.no>
2021-12-08 12:55:13 +0100

    Slettet gamle filer

--- src/String1.kt
+++
@@ -1,6 +0,0 @@
-fun main (args: Array<String>) {
-    println("Skriv inn en streng med space forran og bak")
-    var input = readLine()?:""
-    input = input.trim()
-    println(input)
-}
--- src/VariableString.kt
+++
@@ -1,7 +0,0 @@
-fun main (args: Array<String>) {
-    var input = " Dette er test en. "
-    input = input.trim()
-    println(input)
-    val input2 = input.substring(0,9)+input.substring(14,16)+input.substring(8,13)
-    println(input2.uppercase())
-}
