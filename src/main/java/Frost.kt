225247cf011efdd9d0934e9307340f4bdfd3c9cf
rhetleflaat <roberth@kred.no>
2022-01-07 19:46:08 +0100

    Endringer

--- src/main/java/Frost.kt
+++ src/main/java/Frost.kt
@@ -1,3 +1,4 @@
+import com.google.gson.Gson
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 
@@ -5,8 +6,7 @@ import okhttp3.MediaType.Companion.toMediaTypeOrNull
 fun getJson() {
     val client = OkHttpClient().newBuilder()
         .build()
-    val mediaType: MediaType?
-    mediaType = "application/x-www-form-urlencoded".toMediaTypeOrNull()
+    val mediaType: MediaType? = "application/x-www-form-urlencoded".toMediaTypeOrNull()
     val body: RequestBody = RequestBody.create(
         mediaType,
         "client_id=5435ddce-8016-48d0-b50f-a535f70be85d&client_secret=f6ee8cf8-f634-45ae-bcd9-48bdbc482e87&grant_type=client_credentials"
@@ -19,12 +19,14 @@ fun getJson() {
     val response = client.newCall(request).execute()
 
     val resp : String? = response.body?.string()
-    println(resp);
+    var gson : Gson = Gson()
+    val tokenObj: TokenObj = gson.fromJson<TokenObj>(resp,TokenObj::class.java)
+    println(tokenObj.access_token);
 
     val request2: Request = Request.Builder()
         .url("https://frost.met.no/observations/v0.jsonld?sources=SN50540&referencetime=2021-11-01/2021-11-02&elements=air_temperature")
         .method("GET", null)
-        .addHeader("Authorization", "Bearer fquy-hSPXiW3J2QxIMKantomBvF-uY5bj61NRfQ8yj0=|AAAAAAAAFpkAAAF9qmttlQAAAAI=")
+        .addHeader("Authorization", "Bearer "+tokenObj.access_token)
         .build()
     val response2 = client.newCall(request2).execute()
     println(response2.body?.string())
---
+++ src/main/java/TokenObj.kt
@@ -0,0 +1,1 @@
+data class TokenObj(val access_token: String,val token_type: String,val expires_in: Int)
